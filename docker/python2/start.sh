3ef8fafc8b88e25efa36cb91813e02346658358a
yubang <yubang93@gmail.com>
2015-11-20 23:44:26 +0800

    增加python2.7环境

---
+++ docker/python2/start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 启动脚本
+mkdir -p /var/www/html
+wget $1 -O /tmp/www.zip
+unzip /tmp/www.zip -d /var/www/html
+
+chmod -Rv 555 /var/www
+
+/usr/local/nginx/sbin/nginx
+
+# 调用pip
+cd /var/www/html
+pip install -r requirements.txt
+
+# 启动进程
+gunicorn -b 127.0.0.1:8000 -w 20 index:app
+
+while true
+do
+    sleep 50
+done
