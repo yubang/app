be629171163499a0ab2465a4f2d03c381bd7f6a6
yubang <yubang93@gmail.com>
2017-05-01 17:08:19 +0800

    创建增加页面

---
+++ src/pages/new_app/index.js
@@ -0,0 +1,10 @@
+app.init({
+    data: {
+        form: {
+            image: "static",
+            memory: "64M",
+            cpu: "1核"
+        },
+        options: [{label: "静态资源环境", value: "static"}]
+    }
+})
